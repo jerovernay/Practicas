85acdce9f7968abad676d9ff9597ef87acaaf548
jerovernay <163892693+jerovernay@users.noreply.github.com>
2024-04-08 11:23:50 -0300

    Update test.hs

--- test.hs
+++ test.hs
@@ -1,1 +1,15 @@
-doubleMe = x + x
+f :: Int -> Int
+f 1 = 8
+f 4 = 131
+f 16 = 16
+
+g :: Int -> Int
+g 8 = 16
+g 16 = 4
+g 131 = 1
+
+h :: Int -> Int
+h x = f (g x)
+
+k :: Int -> Int
+k x = g (f x) 
