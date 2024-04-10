e2c0b4b778eeb9ca9ff98ed92abbf3705a6db236
jerovernay <163892693+jerovernay@users.noreply.github.com>
2024-04-09 21:00:19 -0300

    Update G3-ej5.hs

--- G3-ej5.hs
+++ G3-ej5.hs
@@ -1,1 +1,25 @@
+-- Ejercicio 5. Implementar la funci´on todosMenores :: (Integer, Integer, Integer) ->Bool
+--problema todosMenores (t : Z × Z × Z) : Bool {
+--requiere: {True}
+--asegura: {(res = true) ↔ ((f(t0) > g(t0)) ∧ (f(t1) > g(t1)) ∧ (f(t2) > g(t2)))}
 
+--problema f (n: Z) : Z {
+--requiere: {T rue}
+--asegura: {(n ≤ 7 → res = n^2) ∧ (n > 7 → res = 2n − 1)}
+
+
+--problema g (n: Z) : Z {
+--requiere: {T rue}
+--asegura: {Si n es un n´umero par, entonces res = n/2, en caso contrario, res = 3n + 1}
+
+f :: Int -> Int
+f x| x <= 7 = x^2
+   | x > 7 = 2*x -1
+
+g :: Int -> Int
+g x| mod x 2 == 0 = x\2
+   | otherwise = 3*x + 1
+
+todosMenores :: (Int, Int, Int) -> Bool
+todosMenores (x, y, z)| f(x) > g(x) && f(y) > g(y) && f(z) > g(z) = True
+                      | otherwise = False
