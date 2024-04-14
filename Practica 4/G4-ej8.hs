3c5ebd85a4e3605bf49ec6d824fd8c4fd93fca88
jerovernay <163892693+jerovernay@users.noreply.github.com>
2024-04-14 16:51:21 -0300

    Terminar G4-ej8.hs

---
+++ Practica 4/G4-ej8.hs
@@ -0,0 +1,11 @@
+{- Ejercicio 8. Implementar la funcion iesimoDigito :: Integer ->Integer ->Integer que dado un n ∈ N≥0 y un i ∈ N
+menor o igual a la cantidad de dıgitos de n, devuelve el i-´esimo d´ıgito de n. -}
+
+cantDigitos :: Int -> Int
+cantDigitos x 
+             | x < 10 = 1 
+             | otherwise = ultimoDigito (cantDigitos x)
+             where ultimoDigito x = div x 10                                      -- En general me cuesta entender el problema, no encuentro la correlacion entre la formula y cual es mi objetivo
+
+iesimoDigito :: Int -> Int -> Int
+iesimoDigito n i = mod (div n 10^(cantDigitos n - i)) 10                        --No terminado 
