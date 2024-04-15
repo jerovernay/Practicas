d28e0418939ba8e9156173b661dc02e205ba8456
jerovernay <163892693+jerovernay@users.noreply.github.com>
2024-04-15 11:47:50 -0300

    Update G4-ej10.hs

--- Practica 4/G4-ej10.hs
+++ Practica 4/G4-ej10.hs
@@ -7,3 +7,13 @@ f1 n = 2^n + f1 (n - 1)
 f2 :: Int -> Int -> Int
 f2 1 q = q
 f2 n q = q^n + f2 (n - 1) q 
+
+--c)
+f3 :: Int -> Int -> Int
+f3 1 q = q^(2*1) 
+f3 n q = q^(2*n) + f3 (n - 1) q
+
+--d)
+f4 :: Int -> Int -> Int -> Int
+f4 i q n| i == n = q^n  
+        | otherwise = q^i + (f4 (i - 1) q n)
