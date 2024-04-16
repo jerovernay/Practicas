7f36ee7ab29fe5c570cff3a155bb4aa0a9f5f1b1
jerovernay <163892693+jerovernay@users.noreply.github.com>
2024-04-16 19:59:00 -0300

    Create G4-ej18.hs

---
+++ Practica 4/G4-ej18.hs
@@ -0,0 +1,8 @@
+{- Ejercicio 18. Implementar una funcion mayorDigitoPar :: Integer ->Integer segun la siguiente especificacion: -}
+
+
+mayorDigitoPar :: Integer -> Integer
+mayorDigitoPar n
+                | mod n 2 == 0 = n
+                | n == 1 = -1
+                | otherwise = mayorDigitoPar (n - 1)
