b94443ca1465da7008279ffa8559f5d4d07417f4
jerovernay <163892693+jerovernay@users.noreply.github.com>
2024-04-16 17:59:56 -0300

    Update G4-ej16.hs

--- Practica 4/G4-ej16.hs
+++ Practica 4/G4-ej16.hs
@@ -1,1 +1,18 @@
+{- Ejercicio 16. Recordemos que un entero p > 1 es primo si y solo si no existe un entero k tal que 1 < k < p y k divida a p. -}
 
+-- a) Implementar menorDivisor :: Integer ->Integer que calcule el menor divisor (mayor que 1) de un natural n pasado como parametro 
+
+menorDivisor :: Integer ->Integer
+menorDivisor n = divisor n 2
+
+divisor :: Integer -> Integer -> Integer
+divisor n x 
+            | mod n 2 == 0 = 2
+            | otherwise = divisor n (x + 1)
+
+esPrimo :: Integer -> Bool
+esPrimo n = n == menorDivisor n
+
+{- esPrimo :: Integer -> Bool -}
+
+-- ) Implementar la funci´on esPrimo :: Integer ->Bool que indica si un n´umero natural pasado como par´ametro es primo
