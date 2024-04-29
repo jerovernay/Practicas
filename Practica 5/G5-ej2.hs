9242efc8a18c8a961c5476f4f6434ced6ab1bf96
jerovernay <163892693+jerovernay@users.noreply.github.com>
2024-04-29 10:51:09 -0300

    Update G5-ej2.hs

--- Practica 5/G5-ej2.hs
+++ Practica 5/G5-ej2.hs
@@ -10,6 +10,10 @@ principio :: [t] -> [t]
 principio [x] = []
 principio (x:xs) = x : principio xs
 
+reverso :: [t] -> [t]
+reverso [] = []
+reverso (x:xs) = reverso xs ++ [x] 
+
 -- 1. pertenece :: (Eq t) => t -> [t] -> Bool seg´un la siguiente especificaci´on:
 
 pertenece :: (Eq t) => t -> [t] -> Bool
@@ -28,8 +32,64 @@ todosIguales (x:y:xs) | x /= y = False
 -- 3. todosDistintos :: (Eq t) => [t] -> Bool seg´un la siguiente especificaci´on:
 
 todosDistintos :: (Eq t) => [t] -> Bool
-todosDistintos [] = False
+todosDistintos [] = True
 todosDistintos [x] = True
-todosDistintos (x:y:xs) | x == y = False                                                    -- no termino de sacarlo en el caso de que no esten todos a la par
-                        | todosDistintos (y:xs) == todosDistintos xs = False
-                        | otherwise = True
+todosDistintos (x:y:xs) | x == y = False
+                        | otherwise = todosDistintos(y:xs)
+
+--4. hayRepetidos :: (Eq t) => [t] -> Bool seg´un la siguiente especificaci´on:
+
+hayRepetidos :: (Eq t) => [t] -> Bool
+hayRepetidos [] =  False
+hayRepetidos [x] = False
+hayRepetidos (x:y:xs) | x == y = True
+                      | otherwise = hayRepetidos (y:xs)
+
+-- 5. quitar :: (Eq t) => t -> [t] -> [t], que dada una lista xs y un elemento x, elimina la primera aparici´on de x en la lista xs (de haberla).
+
+quitar :: (Eq t) => t -> [t] -> [t]
+quitar _ [] = []
+quitar n (x:xs) | n == x = xs   
+                | n /= x = (x:xs) 
+                | otherwise = quitar n xs
+
+--6. quitarTodos :: (Eq t ) => t -> [t] -> [t], que dada una lista xs y un elemento x, elimina todas las apariciones de x en la lista xs (de haberlas). Es decir:
+
+quitarTodos :: (Eq t ) => t -> [t] -> [t]
+quitarTodos _ [] = []
+quitarTodos n (x:xs) | n == x = quitarTodos n xs
+                     | otherwise = x : quitarTodos n xs 
+
+--7. eliminarRepetidos :: (Eq t) => [t] -> [t] que deja en la lista una ´unica aparici´on de cada elemento, eliminando las repeticiones adicionales
+
+eliminarRepetidos :: (Eq t) => [t] -> [t]
+eliminarRepetidos [] = []
+eliminarRepetidos [x] = [x]
+eliminarRepetidos (x:y:xs) | hayRepetidos(x:y:xs) == False = (x:y:xs)
+                           | x == y = x : quitarTodos x (y:xs)
+                           | otherwise = x : eliminarRepetidos (y:xs)
+
+--8. mismosElementos :: (Eq t) => [t] -> [t] -> Bool, que dadas dos listas devuelve verdadero si y solamente si ambas listas contienen los mismos elementos, sin tener en cuenta repeticiones, es decir:
+
+mismosElementos :: (Eq t) => [t] -> [t] -> Bool
+mismosElementos l1 l2 = mismosElementosAux l1 l1 l2 l2
+
+mismosElementosAux :: (Eq t) => [t] -> [t] -> [t] -> [t] -> Bool
+mismosElementosAux l1 [] l2 [] = True
+mismosElementosAux l1 [] l2 (y:ys)
+                                  | pertenece y l1 = mismosElementosAux l1 [] l2 ys
+                                  | otherwise = False
+mismosElementosAux l1 (x:xs) l2 []
+                                  | pertenece x [] = mismosElementosAux l1 xs l2 []
+                                  | otherwise = False
+mismosElementosAux l1 (x:xs) l2 (y:ys)
+                                      | pertenece x l2 = mismosElementosAux l1 xs l2 (y:ys)
+                                      | otherwise = False
+
+-- 9. capicua :: (Eq t) => [t] -> Bool seg´un la siguiente especificaci´on:
+
+capicua :: (Eq t) => [t] -> Bool
+capicua [] = False
+capicua [x] = True                                      -- Aclaro en caso de que solo haya o un caracter o un numero solo. Como no se la regla general dee capìcua, supongo que es "True", porque se lee de la misma forma para mabos lados
+capicua (x:xs) | reverso (x:xs) == (x:xs) = True
+               | otherwise = False
