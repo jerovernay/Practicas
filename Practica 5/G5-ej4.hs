776f80e33e857cb08f1f79cef746e4a89605832b
jerovernay <163892693+jerovernay@users.noreply.github.com>
2024-05-02 10:39:37 -0300

    Update G5-ej4.hs

--- Practica 5/G5-ej4.hs
+++ Practica 5/G5-ej4.hs
@@ -9,8 +9,8 @@ longitud (x:xs) = 1 + longitud xs
 sacarBlancosRepetidos :: [Char] -> [Char]
 sacarBlancosRepetidos [] = [] 
 sacarBlancosRepetidos [x] = [x]
-sacarBlancosRepetidos (x:y:xs) | x == ' ' && y == ' ' = sacarBlancosRepetidos xs
-                               | otherwise = x : sacarBlancosRepetidos(y:xs)
+sacarBlancosRepetidos (x:xs)| x == head xs && x == ' ' = sacarBlancosRepetidos xs
+                            | otherwise = x : sacarBlancosRepetidos xs
 
 -- b) contarPalabras :: [Char] -> Integer, que dada una lista de caracteres devuelve la cantidad de palabras que tiene. 
 
@@ -34,8 +34,44 @@ sacarPrimerBlanco (x:xs) | x == ' ' = xs
 
 palabras :: [Char] -> [[Char]]
 palabras [] = []
-palabras (x:xs) = sacarPrimerBlanco (x:xs)
+palabras (x:xs) = palabrasAux(sacarBlancosRepetidos (x:xs)) []
 
-palabrasAux :: [Char] -> [[Char]]
-palabrasAux [] = []
-palabrasAux (x:xs) | longitud 
+palabrasAux :: [Char] -> [Char] -> [[Char]]
+palabrasAux [] [] = []
+palabrasAux [] algo = [algo]                        -- me queda el primer blanco dentro de la lista, incluso usando funcion
+palabrasAux (x:xs) algo  
+    | x /= ' ' = palabrasAux xs (algo ++ [x])
+    | otherwise = [algo] ++ palabrasAux xs []
+
+-- d) palabraMasLarga :: [Char] -> [Char], que dada una lista de caracteres devuelve su palabra m´as larga.
+
+comparacionLongitud :: [Char] -> [Char] -> [Char]
+comparacionLongitud w1 w2 
+    | longitud w1 >= longitud w2 = w1
+    | otherwise = w2
+
+palabraMasLarga :: [Char] -> [Char]
+palabraMasLarga l1 = palabraMasLargaAux (palabras l1)
+
+palabraMasLargaAux :: [[Char]] -> [Char]
+palabraMasLargaAux [] = []
+palabraMasLargaAux [algo] = algo
+palabraMasLargaAux (x:xs) = comparacionLongitud x (palabraMasLargaAux xs)
+
+-- e) aplanar :: [[Char]] -> [Char], que a partir de una lista de palabras arma una lista de caracteres concaten´andolas.
+
+aplanar :: [[Char]] -> [Char]
+aplanar [] = []                     -- puede llegar a ser " " 
+aplanar [texto] = texto
+aplanar (x:xs) =  x ++ aplanar xs
+
+-- f)  aplanarConBlancos :: [[Char]] -> [Char], que a partir de una lista de palabras, arma una lista de caracteres concatenandolas e insertando un blanco entre cada palabra
+
+aplanarConBlancos :: [[Char]] -> [Char]
+aplanarConBlancos [] = []
+aplanarConBlancos [x] = x
+aplanarConBlancos (x:xs) = x ++ " " ++ aplanarConBlancos xs
+
+-- g) aplanarConNBlancos :: [[Char]] -> Integer -> [Char], que a partir de una lista de palabras y un entero n, arma una lista de caracteres concaten´andolas e insertando n blancos entre cada palabra (n debe ser no negativo)
+
+
