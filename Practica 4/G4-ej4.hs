feb1547a0688715b45aff99b021970ee99ecaedf
jerovernay <163892693+jerovernay@users.noreply.github.com>
2024-05-05 17:09:35 -0300

    Update G4-ej4.hs

--- Practica 4/G4-ej4.hs
+++ Practica 4/G4-ej4.hs
@@ -1,9 +1,7 @@
 {- Ejercicio 4. Implementar la funci´on sumaImpares :: Integer ->Integer que dado n ∈ N sume los primeros n n´umeros
 impares. Por ejemplo: sumaImpares 3 ❀ 1+3+5 ⇝ 9. -}
 
-sumaImpares :: Int -> Int
-sumaImpares n 
-             | 2*n - 1 <= 0 = 0
-             | 2*n - 1 == 1 = 1
-             | otherwise = sumaImpares n + sumaImpares(n - 1) + sumaImpares(n - 2)
-    -- no me sale!
+sumaImpares :: Int -> Int 
+sumaImpares 0 = 0
+sumaImpares n = impares n + sumaImpares (n - 1)
+  where impares n = 2*n - 1
