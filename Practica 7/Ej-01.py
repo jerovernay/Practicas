0420ddd7ff5cc2b3e56b9cea52a84002c07ac7f3
jerovernay <jeronimo.vernay@gmail.com>
2024-05-25 18:20:07 -0300

    Update Ej-01.py

--- Practica 7/Ej-01.py
+++ Practica 7/Ej-01.py
@@ -46,7 +46,93 @@ def suma_total(s: list) -> int:
             res += s[i]
     return res
 
-print(suma_total(seq1))
-print(suma_total(seq2))
+#print(suma_total(seq1))
+#print(suma_total(seq2))
+
+#4)
 
 
+def ordenados(s: list) -> bool:
+    rs: bool = True
+    i: int = 0
+    while ((i < len(s) - 1) & (rs == True)):
+        if (s[i] > s[i+1]):
+            rs = False
+        i += 1
+    return rs
+
+#print(ordenados(seq1))
+
+#5)
+
+seq3 = ["Hola", "Excelente", "Excelente"]
+
+def es_mayor_a_7(l: list) -> bool:
+    res: bool = False
+    for i in range(0, len(l), 1):
+        if(len(l[i]) > 7):
+            res = True
+            break
+    return res                                      # ENTENDER QUE NUNCA PERO NUNCA SE DEJA EN EL MISMO QUE EL "IF", PEDAZO DE ESTUPIDO.
+
+#print(es_mayor_a_7(seq3))
+
+#6)
+
+def es_palindromo(p: str) -> bool:          #ana anita lava la tina
+    res: bool = True
+    i: int = 0
+    while i < len(p):
+        if p[i] != p[len(p) -1 - i]:
+            res = False
+        i += 1
+    return res
+
+#p: str = input("palabra: ")
+#print(es_palindromo(p))
+
+#7)
+
+def hay_Minus(c:str) -> bool:
+    for char in hay_Minus:
+        if ("a" <= char <= "z"):
+            return True
+        else:
+            return False
+
+def hay_Mayus(c: str) -> bool:
+    for char in hay_Mayus:
+        if ("A" <= char <= "Z"):
+            return True
+        else:
+            return False
+
+def hay_Num(c: str) -> bool:
+    for char in hay_Num:
+        if ("0" <= char <= "9"):
+            return True
+        else:
+            return False
+
+def esVerde(c:str) -> bool:
+    if len(c) > 8:
+        if (hay_Minus(c)):
+            if (hay_Mayus(c)):
+                if (hay_Num(c)):
+                    return True
+    return False
+
+def esRojo(c: str) -> bool:
+    if len(c) < 5:
+        return True
+    return False
+    
+def seguridad(c:str) -> str:
+    if esVerde(c):
+        return "Verde"
+    elif esRojo(c):
+        return "Roja"
+    else:
+        return "Amarilla"
+
+print(seguridad("Sonia2901"))
